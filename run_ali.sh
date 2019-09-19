f4b16a885c5b1ee72f615a13eeb06ae3a6407d3f
Jeff Fischer <jeff@data-ken.org>
2019-09-19 14:12:36 +0200

    add metrics

--- run_ali.sh
+++ run_ali.sh
@@ -5,4 +5,4 @@ else
   EPOCHS=100
 fi
 export KMP_DUPLICATE_LIB_OK=TRUE
-python src/run_with_lineage.py data/real/ali/event-train.txt data/real/ali/time-train.txt data/real/ali/event-test.txt data/real/ali/time-test.txt --epochs=$EPOCHS --train-eval --test-eval --summary ./tfevents  --batch-size 10 --restart --cpu-only
+python src/run_with_lineage.py data/real/ali/event-train.txt data/real/ali/time-train.txt data/real/ali/event-test.txt data/real/ali/time-test.txt --epochs=$EPOCHS --train-eval --test-eval --summary ./tfevents --metrics ./results  --batch-size 10 --restart --cpu-only
